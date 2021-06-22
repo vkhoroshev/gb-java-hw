5054f5de8c65edf4952944fc40abd4e351d0c708
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-06-22 12:05:07 +0300

    Решить п3 из задания
    Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b,
    и инициализируйте их любыми значениями, которыми захотите.
    Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0,
    то вывести в консоль сообщение "Сумма положительная", в противном случае - "Сумма отрицательная".

--- src/quarter/one/level/one/lesson/one/HomeWorkApp.java
+++ src/quarter/one/level/one/lesson/one/HomeWorkApp.java
@@ -3,11 +3,22 @@ package quarter.one.level.one.lesson.one;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
         System.out.println("_Orange");
         System.out.println("_Banana");
         System.out.println("_Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = (int) ((Math.random() * 100) * (Math.random() > 0.5 ? 1 : -1));
+        int b = (int) ((Math.random() * 100) * (Math.random() > 0.5 ? 1 : -1));
+        if ((a + b) >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
