eb7a285dc35d0618cc2ddb14159aa12a416b3478
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-06-22 11:40:45 +0300

    Решить п2 из задания
    Создайте метод printThreeWords(), который при вызове должен
    отпечатать в столбец три слова: _Orange, _Banana, _Apple.

---
+++ src/quarter/one/level/one/lesson/one/HomeWorkApp.java
@@ -0,0 +1,13 @@
+package quarter.one.level.one.lesson.one;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("_Orange");
+        System.out.println("_Banana");
+        System.out.println("_Apple");
+    }
+}
