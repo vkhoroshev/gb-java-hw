441e5713c32e3c2808d344855826a5608f35a079
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-07-12 12:24:06 +0300

    Добавить класс Кот
    Каждому коту нужно добавить поле сытость (когда создаем котов, они голодны).
    Если коту удалось покушать (хватило еды), сытость = true.
    
    Считаем, что если коту мало еды в тарелке, то он её просто не трогает,
    то есть не может быть наполовину сыт (это сделано для упрощения логики программы).

---
+++ src/quarter/one/level/one/lesson/seven/Cat.java
@@ -0,0 +1,47 @@
+package quarter.one.level.one.lesson.seven;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name) {
+        this.name = name;
+        this.appetite = 5;
+    }
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public void eat(Plate plate) {
+        if (plate.decrease(appetite)) {
+            satiety = true;
+            System.out.println(name + " eat...");
+        } else {
+            System.out.println(name + " can't eat!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", appetite=" + appetite +
+                ", satiety=" + satiety +
+                '}';
+    }
+}
