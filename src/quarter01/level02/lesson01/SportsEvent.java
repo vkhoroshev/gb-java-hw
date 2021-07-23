79fe01f216e065f16e8aea121df047f2f73915cf
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-07-23 14:03:54 +0300

    Добавить интерфейс Спортивное событие

---
+++ src/quarter01/level02/lesson01/SportsEvent.java
@@ -0,0 +1,6 @@
+package quarter01.level02.lesson01;
+
+public interface SportsEvent {
+
+    boolean overcomeObstacle(Sportsman sportsman);
+}
--- src/quarter01/level02/lesson01/Wall.java
+++ src/quarter01/level02/lesson01/Wall.java
@@ -1,4 +1,14 @@
 package quarter01.level02.lesson01;
 
-public class Wall {
+public class Wall implements SportsEvent {
+    private final int height;
+
+    public Wall(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public boolean overcomeObstacle(Sportsman sportsman) {
+        return sportsman.jump(height);
+    }
 }
