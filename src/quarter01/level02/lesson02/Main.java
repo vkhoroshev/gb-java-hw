26d1e277e470d6aa93357cf70143aae472642782
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-07-27 14:25:33 +0300

    Добавить суммирование элементов массива с обработкой исключений

--- src/quarter01/level02/lesson02/Main.java
+++ src/quarter01/level02/lesson02/Main.java
@@ -1,7 +1,48 @@
 package quarter01.level02.lesson02;
 
 public class Main {
-    public static void main(String[] args) {
+    static int SIZE = 4;
 
+    public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "1", "2", "3"},
+                {"4", "5", "6", "7"},
+        };
+        try {
+            System.out.println(sum(array));
+        } catch (MyArraySizeException e) {
+            e.printStackTrace();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
+            System.out.println(e.getRow() + " " + e.getCol());
+            System.out.println(array[e.getRow()][e.getCol()]);
+        }
+    }
+
+    static int sum(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != SIZE) {
+            throw new MyArraySizeException();
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != SIZE) {
+                throw new MyArraySizeException();
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("format " + i + " " + j, i, j);
+                }
+            }
+        }
+
+        return sum;
     }
 }
