c23606c5be64883ad368ee9b06f0d128ac461c00
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-10-12 16:55:17 +0300

    Добавить класс Main

--- src/quarter01/level03/lesson01/Main.java
+++ src/quarter01/level03/lesson01/Main.java
@@ -2,6 +2,15 @@ package quarter01.level03.lesson01;
 
 public class Main {
     public static void main(String[] args) {
+        FruitBox<Apple> box1 = new FruitBox<>();
+        box1.add(new Apple(1.0f));
+        box1.add(new Apple(1.0f));
+        box1.add(new Apple(1.0f));
 
+        FruitBox<Orange> box2 = new FruitBox<>(
+                new Orange(1.5f),
+                new Orange(1.5f));
+
+        System.out.println(box1.compare(box2));
     }
 }
