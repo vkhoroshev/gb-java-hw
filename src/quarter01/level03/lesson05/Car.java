91db12c7a49eff1b726b731bb125a9a00233fba2
Vadim Khoroshev <vs.khoroshev@gmail.com>
2021-10-27 10:37:34 +0300

    Синхронизировать потоки и добавить победителя

--- src/quarter01/level03/lesson05/Car.java
+++ src/quarter01/level03/lesson05/Car.java
@@ -1,16 +1,27 @@
 package quarter01.level03.lesson05;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Car implements Runnable {
+    private static AtomicInteger ai;
     private static int CARS_COUNT;
+
+    static {
+        ai = new AtomicInteger(0);
+    }
+
     private Race race;
     private int speed;
     private String name;
+    private CyclicBarrier barrier;
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier barrier) {
         CARS_COUNT++;
         this.race = race;
         this.speed = speed;
         this.name = "Участник #" + CARS_COUNT;
+        this.barrier = barrier;
     }
 
     public String getName() {
@@ -27,12 +38,20 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            barrier.await();
+            barrier.await();
+
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+
+            if (ai.incrementAndGet() == 1) {
+                System.out.println(name + " ПОБЕДИЛ !!!");
+            }
+
+            barrier.await();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
--- src/quarter01/level03/lesson05/Tunnel.java
+++ src/quarter01/level03/lesson05/Tunnel.java
@@ -1,8 +1,12 @@
 package quarter01.level03.lesson05;
 
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+    private Semaphore semaphore;
 
     public Tunnel() {
+        semaphore = new Semaphore(Main.CARS_COUNT / 2);
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
     }
@@ -10,17 +14,18 @@ public class Tunnel extends Stage {
     @Override
     public void go(Car c) {
         try {
-            try {
+            if (!semaphore.tryAcquire()) {
                 System.out.println(c.getName() + " готовится к этапу (ждет): " + description);
-                System.out.println(c.getName() + " начал этап: " + description);
-                Thread.sleep(length / c.getSpeed() * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(c.getName() + " закончил этап: " + description);
+                semaphore.acquire();
             }
-        } catch (Exception e) {
+
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println(c.getName() + " закончил этап: " + description);
+            semaphore.release();
         }
     }
 }
